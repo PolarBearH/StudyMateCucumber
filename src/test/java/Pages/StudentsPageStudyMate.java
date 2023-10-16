c5c7b433347a2aab44283de1a4a72d1b56a03004
Hussein <hassonzy@Husseins-Air.hsd1.il.comcast.net>
2023-10-16 10:33:54 -0500

    Faker Update

--- src/test/java/Pages/StudentsPageStudyMate.java
+++ src/test/java/Pages/StudentsPageStudyMate.java
@@ -9,7 +9,6 @@ import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import utilities.Driver;
-import utilities.TestUtilities;
 
 import java.time.Duration;
 
